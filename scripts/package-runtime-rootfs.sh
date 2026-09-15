f7b3ccb7dd371acf9097f566597f881fbdaba3c4
lbailiang11-droid <lbailiang11@gmail.com>
2026-09-15 08:35:07 +0800

    Update package-runtime-rootfs.sh

--- scripts/package-runtime-rootfs.sh
+++ scripts/package-runtime-rootfs.sh
@@ -40,7 +40,7 @@ docker run --rm --platform linux/386 \
         status=0
         apk --root /target --arch x86 --no-cache add \
             bash ca-certificates coreutils curl findutils git jq less \
-            openssh-client patch python3 ripgrep || status=$?
+            openssh-client openrc patch python3 ripgrep || status=$?
         if ! chown -R "$HOST_UID:$HOST_GID" /target; then
             exit 1
         fi
